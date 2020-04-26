3ea1ddde666b2d4011c4b7869ff5428220f26d18
eusGithubwork <61106313+eusGithubwork@users.noreply.github.com>
2020-04-26 22:06:54 +0530

    day 2020_04_26

---
+++ code/2020_04_26/Code.java
@@ -0,0 +1,10 @@
+public class Code{
+	public static void main(String []args)
+	{
+		System.out.print(67.8);
+		System.out.print("Hello");
+		System.out.print(90);	
+		System.out.print('@');
+	}
+}
+
