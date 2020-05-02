a9a369c30377649d9656be330b5d1f29aba21dad
eusGithubwork <61106313+eusGithubwork@users.noreply.github.com>
2020-05-02 21:39:24 +0530

    questions updated 02_05_2020

---
+++ Code/29_04_2020/Code.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Code{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.print("Enter a number : ");
+		num = sc.nextInt();
+		System.out.print("You Entered : "+num);
+	}
+}
+
+sc.nextByte();
+sc.nextShort();
+sc.nextInt();
+sc.nextLong();
+sc.nextBoolean();
+sc.nextFloat();
+sc.nextDouble();
+sc.nextLine().chatAt(0);
---
+++ javaCodeSolution/ques0010/Code.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Code{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		float radius, circum;
+		System.out.print("Enter radius of circle : ");
+		radius = sc.nextFloat();
+		circum = 2*3.14f*radius;
+		System.out.print("circumference of circle : "+circum);
+	}
+}
