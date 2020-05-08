46480f563413fe1a0083a2c9e4016bd60b4b2a2c
eusGithubwork <61106313+eusGithubwork@users.noreply.github.com>
2020-05-08 20:44:15 +0530

    questions

---
+++ javaCodeSolution/ques0002/Code.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Code{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		byte byteNum;
+		short shortNum;
+		int intNum;
+		long longNum;
+		float floatNum;
+		double doubleNum;
+		char ch;
+		boolean flag;
+
+		System.out.print("Enter byteNum : ");
+		byteNum = sc.nextByte();
+
+		System.out.print("Enter shortNum : ");
+		shortNum = sc.nextShort();
+
+		System.out.print("Enter intNum : ");
+		intNum = sc.nextInt();
+
+		System.out.print("Enter longNum : ");
+		longNum = sc.nextLong();
+
+		System.out.print("Enter floatNum : ");
+		floatNum = sc.nextFloat();
+
+		System.out.print("Enter doubleNum : ");
+		doubleNum = sc.nextDouble();
+
+		System.out.print("Enter boolean value : ");
+		flag = sc.nextBoolean();
+
+		System.out.print("Enter char value : ");
+		ch = sc.next().charAt(0);
+
+		System.out.print("byteNum : "+byteNum+"\n");
+		System.out.print("shortNum : "+shortNum+"\n");
+		System.out.print("intNum : "+intNum+"\n");
+		System.out.print("longNum : "+longNum+"\n");
+		System.out.print("floatNum : "+floatNum+"\n");
+		System.out.print("doubleNum : "+doubleNum+"\n");
+		System.out.print("char value : "+ch+"\n");
+		System.out.print("flag value : "+flag+"\n");
+	}
+}
---
+++ javaCodeSolution/ques0021/Code.java
@@ -0,0 +1,17 @@
+public class Code{
+	public static void main(String []args)
+	{
+		int x=10;
+		int y=20;
+		int t;
+		System.out.print("Value Before Swap:\n");
+		System.out.print("x = "+x+"		y = "+y+"\n");
+
+		t=x;
+		x=y;
+		y=t;
+
+		System.out.print("Value After Swap:\n");
+		System.out.print("x = "+x+"		y = "+y+"\n");
+	}
+}
