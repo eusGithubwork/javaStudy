648fb136098f12760cc42074e8897ece5bb7dbbd
eusGithubwork <61106313+eusGithubwork@users.noreply.github.com>
2020-05-13 03:25:01 +0530

    qd

---
+++ javaCodeSolution/ques0023/Code.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Code{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x;
+		int y;
+		System.out.print("Enter x : ");
+		x = sc.nextInt();
+		System.out.print("Enter y : ");
+		y = sc.nextInt();
+		System.out.println("Before Swap :");
+		System.out.println("x : "+x+"		y : "+y);
+
+		x = x*y;
+		y = x/y;
+		x=	x/y;
+
+		System.out.println("After Swap :");
+		System.out.println("x : "+x+"		y : "+y);
+	}
+}
